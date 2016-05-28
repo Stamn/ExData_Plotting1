fa0cbcb9e5c29795e0f0467bcb41f99b406459b9
Stamn <52628144@qq.com>
2016-05-29 00:50:17 +0800

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -12,7 +12,8 @@ plot(ref_time, nrt$Global_active_power, type='l', xlab="", ylab="Globle Active P
 plot(ref_time, nrt$Sub_metering_1, type='l', xlab="", ylab="Energy sub metering")
 lines(ref_time,nrt$Sub_metering_2, col='red')
 lines(ref_time,nrt$Sub_metering_3, col='blue')
-legend("topright", lty=c(1,1,1), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"))
+legend("topright", lty=c(1,1,1), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"),
+      bty='n')
 with(nrt, plot(ref_time, Voltage, type='l', xlab="datetime"))
 with(nrt, plot(ref_time, Global_reactive_power, type='l', xlab="datetime"))
 dev.off()
